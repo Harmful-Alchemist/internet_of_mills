6e9d5e6950db38294362e9938b503343f948d320
Harm <el.harmo@gmail.com>
2018-06-01 21:14:25 +0200

    oops that was silly

--- internet_of_mills/lib/internet_of_mills/peripheral/mill.ex
+++ internet_of_mills/lib/internet_of_mills/peripheral/mill.ex
@@ -1,7 +1,7 @@
 defmodule InternetOfMills.Peripheral.Mill do
   use Ecto.Schema
   import Ecto.Changeset
-
+  alias InternetOfMills.Repo
 
   schema "mills" do
     field :io_pin, :integer
@@ -18,6 +18,6 @@ defmodule InternetOfMills.Peripheral.Mill do
     |> validate_required([:name, :type, :io_pin])
     |> validate_number(:io_pin, greater_than: 1, message: "The pins on the raspberry pi 3 are 2 through 26, so please use a number greater than 1")
     |> validate_number(:io_pin, less_than: 27, message: "The pins on the raspberry pi 3 are 2 through 26, so please use a number less than 27")
-    |> unique_constraint(:io_pin)
+    |> unsafe_validate_unique(:io_pin, Repo)
   end
 end
